1e7f205393cde139e73a8405506cb4ccaf9e60cc
Aigera <aikoaliask@gmail.com>
2023-05-10 18:34:31 +0600

    first commit

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,9 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Aika Ali on 04.05.2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ BMI Calculator/Models/CalculatorBrain.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by Aika Ali on 03.05.2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
